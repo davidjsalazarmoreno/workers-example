2e48643a559e446cd1206aae63ec1277c6fb1d55
David Salazar <davidjsalazarmoreno@gmail.com>
2019-04-27 19:35:03 -0500

    adds fibonacci implementation inside worker

--- worker.js
+++ worker.js
@@ -1,8 +1,15 @@
-self.addEventListener('message', (event) => {
-   console.log(event.data);
+function fibonacci(num) {
+   if (num <= 1) return 1;
+ 
+   return fibonacci(num - 1) + fibonacci(num - 2);
+ }
+ 
 
-   if (event.data === 'getRandomFibonnaci') {
-      const heavyComputedValue = fibonacci(43);
+self.addEventListener('message', (event) => {
+   console.log('Web Worker: ', event.data);
+
+   if (event.data.type === 'getFibonacci') {
+      const heavyComputedValue = 'The result is ' + fibonacci(event.data.payload);
       postMessage(heavyComputedValue);
    }
 
